2d31489107f1159fc26b1e919113e5d484ae9576
AiNakow <ainakowalice@outlook.com>
2025-03-06 17:59:27 +0800

    initial

--- include/connectionBase.h
+++ include/connectionBase.h
@@ -3,12 +3,26 @@
 
 namespace databaseConnection
 {
+    typedef enum
+    {
+        MYSQL,
+        ORACLE,
+        POSTGRESQL,
+        SQLITE
+    } ConnectionType;
+    
     class ConnectionBase
     {
     public:
         virtual void connect() = 0;
         virtual void disconnect() = 0;
+        virtual void query() = 0;
+        virtual void update() = 0;
         virtual bool isConnected() = 0;
+        void getUnusedTime();
+        void refreshUnusedTime();
+    private:
+        long unusedTime;
     };
 } // namespace databaseConnection
 
